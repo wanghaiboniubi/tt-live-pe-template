d3c44198d616574b413a8839caefb6612935585c
wanghaibo <51687229+wanghaiboniubi@users.noreply.github.com>
2026-06-18 02:13:46 +0800

    Update tt-live-pe-template.meta.js

--- tt-live-pe-template.meta.js
+++ tt-live-pe-template.meta.js
@@ -8,6 +8,6 @@
 // @match        https://dataleap-sg.tiktok-row.net/dorado/*
 // @match        https://dataleap-oceanus.tiktok-row.net/dorado/*
 // @match        https://aide.tiktok-row.net/*
-// @downloadURL  https://code.byted.org/live/Prompt_Engineering_Live/raw/main/tt-live-pe-template.user.js
-// @updateURL    https://code.byted.org/live/Prompt_Engineering_Live/raw/main/tt-live-pe-template.meta.js
+// @downloadURL  https://github.com/wanghaiboniubi/tt-live-pe-template/raw/refs/heads/main/tt-live-pe-template.user.js
+// @updateURL    https://github.com/wanghaiboniubi/tt-live-pe-template/raw/refs/heads/main/tt-live-pe-template.meta.js
 // ==/UserScript==
